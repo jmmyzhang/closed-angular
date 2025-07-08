106562bfeeeb2916efbd2be5c20815b496b1da36
jmmyzhang <jimmy.zhang92@gmail.com>
2025-07-08 12:09:30 -0400

    fix server.js file

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const cors = require('cors');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-// Serve Angular static files from dist folder (build your Angular app first)
-app.use(express.static(path.join(__dirname, '../dist/my-angular-app')));
+// Serve Angular static files from dist folder (adjust path if your Angular app name differs)
+app.use(express.static(path.join(__dirname, '../dist/Closed/browser')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/my-angular-app/index.html'));
+app.get('/{*any}', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/Closed/browser/index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
