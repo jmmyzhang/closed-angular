e1f5dc3072427b519c361ba5b40949ff8842a174
jmmyzhang <jimmy.zhang92@gmail.com>
2025-07-08 14:15:59 -0400

    styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,10 @@
 import { Routes } from '@angular/router';
-import { BaseComponent } from './views/base/base-component';
 import { HomeComponent } from './views/home/home-component/home-component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: BaseComponent,
-        children: [
-            {
-                path: 'home',
-                component: HomeComponent
-            }
-        ]
+        component: HomeComponent,
     },
     {
         path: '**',
