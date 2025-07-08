b8f8ed32452a329c077fdf6f97aec92211238d9e
jmmyzhang <jimmy.zhang92@gmail.com>
2025-07-08 13:11:21 -0400

    asd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { BaseComponent } from './views/base/base-component';
+import { HomeComponent } from './views/home/home-component/home-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: BaseComponent,
+        children: [
+            {
+                path: 'home',
+                component: HomeComponent
+            }
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
---
+++ src/app/views/base/base-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-base',
+  imports: [RouterModule],
+  templateUrl: './base-component.html',
+  styleUrl: './base-component.scss'
+})
+export class BaseComponent {
+
+}
